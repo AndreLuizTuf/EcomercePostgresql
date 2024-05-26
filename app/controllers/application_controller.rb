ac7153151f0c4caf5778b0d301cf0e1596ed2370
Andre Luiz <andre.luiz.a.f@hotmail.com>
2024-05-26 09:48:02 -0300

    muitos para muitos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :valida_sessao
+  
+  def valida_sessao
+    x = ""
+  end
 end
--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,31 +1,124 @@
-# Certifique-se de reiniciar o servidor quando modificar este arquivo.
+# config/initializers/inflections.rb
 
-# Adicione novas regras de inflexão usando o seguinte formato. As inflexões
-# são específicas do idioma e você pode definir regras para quantos idiomas
-# desejar. Todos esses exemplos estão ativados por padrão:
 ActiveSupport::Inflector.inflections do |inflect|
   inflect.clear
-  
-  inflect.plural /([r|s|z])$/i, '\1es'
-  inflect.plural /al$/i, 'ais'
-  inflect.plural /el$/i, 'eis'
-  inflect.plural /ol$/i, 'ois'
-  inflect.plural /ul$/i, 'uis'
-  inflect.plural /il$/i, 'is'
 
-  inflect.singular /([r|s|z])es$/i, '\1'
-  inflect.singular /al$/i, 'al'
-  inflect.singular /el$/i, 'el'
-  inflect.singular /ol$/i, 'ol'
-  inflect.singular /ul$/i, 'ul'
-  inflect.singular /il$/i, 'il'
+  #general rule: add "s" to the end of the word
+  #casa - casas
+  inflect.plural /^([a-zA-z]*)a$/i, '\1as'
+  #pe - pes
+  inflect.plural /^([a-zA-z]*)e$/i, '\1es'
+  #no example
+  inflect.plural /^([a-zA-z]*)i$/i, '\1is'
+  #carro - carros
+  inflect.plural /^([a-zA-z]*)o$/i, '\1os'
+  #pneu - pneus
+  inflect.plural /^([a-zA-z]*)u$/i, '\1us'
 
-  inflect.irregular 'pais', 'países'
+  #if word ends in "r" or "z", add "es"
+  #luz - luzes
+  #flor - flores
+  #arroz - arrozes
+  inflect.plural /^([a-zA-z]*)r$/i, '\1res'
+  inflect.plural /^([a-zA-z]*)z$/i, '\1zes'
 
-  inflect.uncountable %w( pais )
+  #if word ends in "al", "el", "ol", "ul": trade "l" with "is"
+  #farol - farois
+  #hospital - hospitais
+  #telemovel - telemoveis
+  #pincel - pinceis
+  #anzol - anzois
+  inflect.plural /^([a-zA-z]*)al$/i, '\1ais'
+  inflect.plural /^([a-zA-z]*)el$/i, '\1eis'
+  inflect.plural /^([a-zA-z]*)ol$/i, '\1ois'
+  inflect.plural /^([a-zA-z]*)ul$/i, '\1uis'
+
+  #if word ends in "il" and has tónic accent in last syllable, trade "il" with "is"
+  #cantil - cantis
+  inflect.plural /^([a-zA-z]*)il$/i, '\1is'
+
+  #TODO
+  #if word ends in "il" and has tónic accent in penultimate syllable, trade "il" with "eis"
+  #no example
+
+  #if word ends in "m", trade "m" with "ns"
+  #armazem - armazens
+  #portagem - portagens
+  inflect.plural /^([a-zA-z]*)m$/i, '\1ns'
+
+  #TODO
+  #if word ends in "s" and has one silable, trade "s" with "es"
+  #no example
+  #inflect.plural /^([a-zA-z]*)e$/i, '\1es'
+
+  #TODO
+  #if word ends in "x" stays the same
+  #no example... professor X, maybe?
+  #inflect.plural /^([a-zA-z]*)x$/i, '\1xes'
+
+  #if word ends in "ão", there are three ways of plural: ãos, ães, ões
+  #NOTE: hard to detect, so I'll use the most common case
+  #and then use irregular cases for the others.  if someone knows of
+  #more cases please add to the list &amp;amp;amp;amp; mail me. thanks!
+  #
+  #cão - cães
+  #colchão - colchões
+  #portão - portões
+  #pão - pães
+  #alemão - alemães
+  #chão - ?
+  #pilhão - pilhões
+  #canhão - canhões
+  #bidão - bidões
+  #mão - mãos
+  inflect.plural /^([a-zA-z]*)ao$/i, '\1oes'
+  # inflect.irregular 'cao', 'caes'
+  # inflect.irregular 'pao', 'paes'
+  #inflect.irregular 'mao', 'maos'
+  #inflect.irregular 'alemao', 'alemaes'
+
+  ############################
+  #singularize rules         #
+  ############################
+
+  #pes - pe
+  #carros - carro
+  #pneus - pneu
+  inflect.singular /^([a-zA-z]*)as$/i, '\1a'
+  inflect.singular /^([a-zA-z]*)es$/i, '\1e'
+  inflect.singular /^([a-zA-z]*)is$/i, '\1i'
+  inflect.singular /^([a-zA-z]*)os$/i, '\1o'
+  inflect.singular /^([a-zA-z]*)us$/i, '\1u'
+
+  #luzes - luz
+  #flores - flor
+  #arrozes - arroz
+  inflect.singular /^([a-zA-z]*)res$/i, '\1r'
+  inflect.singular /^([a-zA-z]*)zes$/i, '\1z'
+
+  #cantis - cantil
+  inflect.singular /^([a-zA-z]*)is$/i, '\1il'
+
+  #farois - farol
+  #hospitais - hospital
+  #telemoveis - telemovel
+  #pinceis - pincel
+  #anzois - anzol
+  inflect.singular /^([a-zA-z]*)ais$/i, '\1al'
+  inflect.singular /^([a-zA-z]*)eis$/i, '\1el'
+  inflect.singular /^([a-zA-z]*)ois$/i, '\1ol'
+  inflect.singular /^([a-zA-z]*)uis$/i, '\1ul'
+
+  #armazens - armazem
+  #portagens - portagem
+  inflect.singular /^([a-zA-z]*)ns$/i, '\1m'
+
+  #cães - cão
+  #colchões - colchão
+  #portões - portão
+  #pães - pão
+  #alemães - alemão
+  inflect.singular /^([a-zA-z]*)oes$/i, '\1ao'
+  inflect.singular /^([a-zA-z]*)aes$/i, '\1ao'
+  inflect.singular /^([a-zA-z]*)aos$/i, '\1ao'
 end
-
-# Essas regras de inflexão são suportadas, mas não ativadas por padrão:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym "RESTful"
-# end
